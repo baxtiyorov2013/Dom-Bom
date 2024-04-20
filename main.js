4f3c8b672f144aa3419a8faeb7d58be72126284a
baxtiyorov2013 <baxtiyorov2013>
2024-04-20 19:18:47 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+const bgColor = document.querySelector("body");
+const randdom = document.querySelector("h1");
+
+let random = Math.floor(Math.random() * 100);
+
+randdom.style.color = "red";
+bgColor.style.background ="black";
+bgColor.style.display ="flex";
+bgColor.style.justifyContent ="center";
+bgColor.style.alignItems = 'center';
+randdom.style.fontSize ="70px";
+randdom.style.fontFamily ="nosifer";
+
+randdom.innerHTML = random;
+ 
